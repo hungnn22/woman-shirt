d3011fcdd0d5e27c92f2222598962e0dc54a97b2
hungnn22 <hungnnit98@gmail.com>
2022-06-12 23:40:34 +0700

    update api order detail

--- ws-server/master-server/src/main/java/com/ws/masterserver/utils/constants/enums/PromotionTypeEnum.java
+++ ws-server/master-server/src/main/java/com/ws/masterserver/utils/constants/enums/PromotionTypeEnum.java
@@ -4,20 +4,32 @@ package com.ws.masterserver.utils.constants.enums;
  * @author myname
  */
 public enum PromotionTypeEnum {
+
+    /**
+     * @code TYPE1
+     * @tittle SHIP
+     * @name Vận chuyển
+     * */
     TYPE1("SHIP", "Vận chuyển"),
+
+    /**
+     * @code TYPE2
+     * @tittle SHOP
+     * @name Mua sắm
+     * */
     TYPE2("SHOP", "Mua sắm")
         ;
 
-    private final String id;
+    private final String tittle;
     private final String name;
 
     PromotionTypeEnum(String id, String name) {
-        this.id = id;
+        this.tittle = id;
         this.name = name;
     }
 
-    public String getId() {
-        return id;
+    public String getTittle() {
+        return tittle;
     }
 
     public String getName() {
