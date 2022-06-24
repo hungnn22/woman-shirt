5793e71b6c661e45880825f918d068522443eb56
hungnn22 <hungnnit98@gmail.com>
2022-06-24 17:50:40 +0700

    123

--- ws-web/admin-web/src/component/topbar/Topbar.jsx
+++ ws-web/admin-web/src/component/topbar/Topbar.jsx
@@ -141,7 +141,7 @@ const Topbar = () => {
                     <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                         <a className="dropdown-item" href="#">
                             <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400" />
-                            Profile
+                            Cá nhân
                         </a>
                         <a className="dropdown-item" href="#">
                             <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400" />
@@ -154,7 +154,7 @@ const Topbar = () => {
                         <div className="dropdown-divider" />
                         <NavLink to='/login' className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                             <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
-                            Logout
+                            Đăng xuất
                         </NavLink>
                     </div>
                 </li>
