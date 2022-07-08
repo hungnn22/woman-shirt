7a6c8c002e622942120c7bbfca1088278c30dd02
hungnn22 <hungnnit98@gmail.com>
2022-07-08 10:07:11 +0700

    refactor entity

--- ws-server/master-server/src/main/java/com/ws/masterserver/entity/ExchangeEntity.java
+++ ws-server/master-server/src/main/java/com/ws/masterserver/entity/ExchangeEntity.java
@@ -1,30 +1,31 @@
 package com.ws.masterserver.entity;
 
-import com.ws.masterserver.utils.constants.enums.MediaType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "review_media")
+@Table(name = "exchange")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class ReviewMediaEntity {
+public class ExchangeEntity {
     @Id
     private String id;
 
-    private String url;
+    @Column(name = "order_id")
+    private String orderId;
 
-    @Enumerated(EnumType.STRING)
-    private MediaType type;
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date")
+    private Date createdDate;
 
-    @Column(name = "review_id")
-    private String reviewId;
+    private String createdBy;
 }
