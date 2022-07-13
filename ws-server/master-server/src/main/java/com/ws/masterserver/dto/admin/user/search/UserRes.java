950866066e9ddb619a8833b3777ab18774990be7
hungnn22 <hungnnit98@gmail.com>
2022-07-13 17:25:05 +0700

    admin user detail

--- ws-server/master-server/src/main/java/com/ws/masterserver/dto/admin/user/search/UserRes.java
+++ ws-server/master-server/src/main/java/com/ws/masterserver/dto/admin/user/search/UserRes.java
@@ -1,10 +1,14 @@
 package com.ws.masterserver.dto.admin.user.search;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRes {
     private String id;
     private String name;
