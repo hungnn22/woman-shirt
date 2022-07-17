5efd3d54e45fe978a8895053ff027d3689e53634
hungnn22 <hungnnit98@gmail.com>
2022-07-18 00:04:44 +0700

    update swagger

--- ws-server/master-server/src/main/java/com/ws/masterserver/controller/admin/AdminReportController.java
+++ ws-server/master-server/src/main/java/com/ws/masterserver/controller/admin/AdminReportController.java
@@ -1,7 +1,14 @@
 package com.ws.masterserver.controller.admin;
 
+import com.ws.masterserver.dto.admin.report.RevenueReq;
+import com.ws.masterserver.utils.base.WsController;
+import com.ws.masterserver.utils.common.JsonUtils;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin/report")
 @RequiredArgsConstructor
 @Slf4j
-public class AdminReportController {
+public class AdminReportController extends WsController {
 
+    @PostMapping("/revenue")
+    @Operation(summary = "báo cáo doanh thu")
+    public ResponseEntity<Object> revenue(@RequestBody RevenueReq req) {
+        log.info("start api POST: /api/v1/admin/report/revenue with payload: {}", JsonUtils.toJson(req));
+        return ResponseEntity.ok(service.adminReportService.revenue(getCurrentUser(), req));
+    }
 
 }
