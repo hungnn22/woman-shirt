434c8f77f731cd9a112e68f66775345c33be1461
hungnn22 <hungnnit98@gmail.com>
2022-07-20 21:50:35 +0700

    revert revert

---
+++ ws-server/master-server/src/main/java/com/ws/masterserver/service/impl/DiscountCustomerTypeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ws.masterserver.service.impl;
+
+import com.ws.masterserver.service.DiscountCustomerTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiscountCustomerTypeServiceImpl implements DiscountCustomerTypeService {
+}
